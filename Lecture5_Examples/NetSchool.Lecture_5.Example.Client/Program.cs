86eb99998337e754a3dceef923dd2502bc6f6383
andrey.kononov <andrey.kononov@dsr-corporation.com>
2023-10-30 20:27:34 +0300

    Add examples for the lecture 5

---
+++ Lecture5_Examples/NetSchool.Lecture_5.Example.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using NetSchool.Lecture_5.Example.RabbitMQ;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace NetSchool.Lecture_5.Example.Client
+{
+    class Program
+    {
+        private static readonly string queueName = "LECTURE5_SAMPLE";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CLIENT");
+
+            var rmq = new RabbitMqService();
+            rmq.RegisterListener(queueName, onReceive: (sender, arg) =>
+            {
+                var message = Encoding.UTF8.GetString(arg.Body.ToArray());
+                Console.Write($"Data: {message}... ");
+                Thread.Sleep(1000);
+                Console.WriteLine($"done");
+            });
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter text (\"exit\" for stop): ");
+                var s = Console.ReadLine();
+                if (s == "exit") break;
+            }
+        }
+    }
+}
---
+++ Lecture5_Examples/NetSchool.Lecture_5.Example.Sender/Program.cs
@@ -0,0 +1,26 @@
+﻿using NetSchool.Lecture_5.Example.RabbitMQ;
+using System;
+
+namespace NetSchool.Lecture_5.Example.Sender
+{
+    class Program
+    {
+        private static readonly string queueName = "LECTURE5_SAMPLE";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SENDER");
+
+            var rmq = new RabbitMqService();
+
+            while (true)
+            {
+                Console.Write("Enter text (\"exit\" for stop): ");
+                var s = Console.ReadLine();
+                if (s == "exit") break;
+
+                rmq.Publish(queueName, s);
+            }
+        }
+    }
+}
