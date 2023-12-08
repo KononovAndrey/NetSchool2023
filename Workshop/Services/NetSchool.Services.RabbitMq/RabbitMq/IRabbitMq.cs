d99b6bda77bc14d2af144b32446ad60f0c6e2c78
andrey.kononov <andrey.kononov@dsr-corporation.com>
2023-12-08 22:11:28 +0300

    Added lost files

---
+++ Workshop/Services/NetSchool.Services.RabbitMq/RabbitMq/IRabbitMq.cs
@@ -0,0 +1,12 @@
+﻿namespace NetSchool.Services.RabbitMq;
+
+using System.Threading.Tasks;
+
+public delegate Task OnDataReceiveEvent<T>(T data);
+
+public interface IRabbitMq
+{
+    Task Subscribe<T>(string queueName, OnDataReceiveEvent<T> onReceive);
+
+    Task PushAsync<T>(string queueName, T data);
+}
